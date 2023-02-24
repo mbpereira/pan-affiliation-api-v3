6f7440231be6329c5ae57a3f2356557faa32fd2f
= <=>
2023-02-24 17:53:11 -0400

    first commit

---
+++ src/main/java/pan/affiliation/AffiliationApplication.java
@@ -0,0 +1,13 @@
+package pan.affiliation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AffiliationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AffiliationApplication.class, args);
+	}
+
+}
