e7e1cfd71c924967443f33f8895aead1d69c8265
= <=>
2023-02-25 14:30:18 -0400

    ajustando query exception

--- src/main/java/pan/affiliation/shared/exceptions/QueryException.java
+++ src/main/java/pan/affiliation/shared/exceptions/QueryException.java
@@ -8,7 +8,7 @@ import lombok.Setter;
 public class QueryException extends Exception {
     private String errorCode;
 
-    public QueryException(String message, String errorCode) {
+    public QueryException(String errorCode, String message) {
         super(message);
         this.errorCode = errorCode;
     }
